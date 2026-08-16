078b89d4f2b8a27af39259de4ada1ad0e89f71c3
Thiago <thiagom.devsec@gmail.com>
2026-08-16 20:32:40 -0300

    feat: bootstrap inventory management backend

---
+++ backend/src/test/java/io/thiiagoms/ims/ImsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thiiagoms.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
