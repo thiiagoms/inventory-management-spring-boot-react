ad697dc80f907039c0373e724fa018ad6c5d6281
thiiagoms <thiagom.devsec@gmail.com>
2026-08-19 22:05:03 -0300

    test(user): wip registration API tests

---
+++ backend/src/test/java/io/thiiagoms/ims/fixtures/user/presentation/http/api/v1/UserApiTestSupport.java
@@ -0,0 +1,24 @@
+package io.thiiagoms.ims.fixtures.user.presentation.http.api.v1;
+
+import io.thiiagoms.ims.fixtures.shared.presentation.http.ApiTestSupport;
+import io.thiiagoms.ims.fixtures.user.presentation.http.api.v1.register.RegisterUserRequestBuilder;
+import io.thiiagoms.ims.user.presentation.http.api.v1.register.RegisterUserRequest;
+
+public class UserApiTestSupport {
+
+    public static final String ENDPOINT = "/api/users";
+
+    private final ApiTestSupport apiTestSupport;
+
+    public UserApiTestSupport(ApiTestSupport apiTestSupport) {
+        this.apiTestSupport = apiTestSupport;
+    }
+
+    public String createUserAndReturnId() throws Exception {
+        return createUserAndReturnId(RegisterUserRequestBuilder.start().build());
+    }
+
+    public String createUserAndReturnId(RegisterUserRequest request) throws Exception {
+        return apiTestSupport.postJsonAndReturnId(ENDPOINT, request);
+    }
+}
---
+++ backend/src/test/java/io/thiiagoms/ims/user/presentation/http/api/v1/register/RegisterUserApiTest.java
@@ -0,0 +1,9 @@
+package io.thiiagoms.ims.user.presentation.http.api.v1.register;
+
+import io.thiiagoms.ims.fixtures.shared.presentation.http.ApiTestSupport;
+import io.thiiagoms.ims.fixtures.user.presentation.http.api.v1.UserApiTestSupport;
+
+public class RegisterUserApiTest extends ApiTestSupport {
+
+    private final UserApiTestSupport userApiTestSupport;
+}
