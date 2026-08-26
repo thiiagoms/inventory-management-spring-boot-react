0b1693c76639897b9687ef7f665183cda9a5f33d
Thiago <thiagom.devsec@gmail.com>
2026-08-26 16:15:57 -0300

    refactor(user): inherit common JPA entity fields

--- backend/src/main/java/io/thiiagoms/ims/shared/infrastructure/persistence/model/BaseJpaEntity.java
+++ backend/src/main/java/io/thiiagoms/ims/shared/infrastructure/persistence/model/BaseJpaEntity.java
@@ -4,25 +4,29 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
-@Setter
+@SuperBuilder
 @MappedSuperclass
-public class BaseJpaEntity {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class BaseJpaEntity {
 
   @Id
-  @Column(name = "id", nullable = false, updatable = false, length = 36)
-  private String id;
+  @Column(name = "id", nullable = false, updatable = false, columnDefinition = "BINARY(16)")
+  private UUID id;
 
   @CreationTimestamp
   @Column(name = "created_at", nullable = false, updatable = false)
   private Instant createdAt;
 
   @UpdateTimestamp
-  @Column(name = "updated_at", nullable = false, updatable = true)
+  @Column(name = "updated_at", nullable = false)
   private Instant updatedAt;
 }
